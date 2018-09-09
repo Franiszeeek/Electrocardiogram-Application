20ca8b529e60dfe1e120b0e6b07b5254882b1942
Maciej Mysiński <Franciszeeek@MacBook-Air-Maciej.local>
2018-09-09 23:20:44 +0200

    application without a chart
    connecting with the device,
    receiving a signal,
    data display.

--- app/src/main/java/com/example/franciszeeek/electrocardiogramapplication/MainActivity.java
+++ app/src/main/java/com/example/franciszeeek/electrocardiogramapplication/MainActivity.java
@@ -3,11 +3,189 @@ package com.example.franciszeeek.electrocardiogramapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.button1)
+    void OnClick_F1() {
+
+    }
+
+    @OnClick(R.id.button2)
+    void OnClick_F2() {
+
+    }
+
+    @OnClick(R.id.button3)
+    void OnClick_F3() {
+
+    }
+
+    @BindView(R.id.txtView1)
+    TextView txtView1;
+
+
+    @BindView(R.id.graph)
+    GraphView graph;
+
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter btAdapter = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final String TAG = "MY_APP_DEBUG_TAG";
+
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private StringBuilder recDataString = new StringBuilder();
+    private ConnectedThread mConnectedThread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+                    String readMessage = (String) msg.obj;
+                    int endOfLineIndex = readMessage.indexOf("~");
+                    if (endOfLineIndex > 0) {
+                        String dataInPrint = readMessage.substring(0, endOfLineIndex);
+                        if (dataInPrint.charAt(0) == '#')								//if it starts with # we know it is what we are looking for
+                        {
+                            String data = dataInPrint.substring(1, endOfLineIndex);
+                            txtView1.setText(data);
+                        }
+                    }
+                }
+            }
+        };
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        return  device.createRfcommSocketToServiceRecord(myUUID);
+    }
+
+    public void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        address = intent.getStringExtra(DeviceList.EXTRA_ADDRESS);
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_LONG).show();
+        }
+        try
+        {
+            btSocket.connect();
+        } catch (IOException e) {
+            try
+            {
+                btSocket.close();
+            } catch (IOException e2) {
+                Log.e(TAG, "Error", e2);            }
+        }
+
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+        mConnectedThread.write("x");
+    }
+
+    private void checkBTState() {
+
+        if(btAdapter==null) {
+            Toast.makeText(getBaseContext(), "Device does not support bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error", e);
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);
+                    String readMessage = new String(buffer, 0, bytes);
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            btSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error", e);
+
+        }
     }
 }
